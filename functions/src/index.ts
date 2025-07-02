919db3da7729a8f6b863da6af4aa0de2bbe1605f
tulionemedeazevedo <tulionemedeazevedo@unilingo.co>
2025-07-02 20:36:37 -0300

    feat(functions): add translatorOnUpdate to translate transcript using GPT and update Firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as admin from "firebase-admin";
 import {transcribeOnCreate} from "./transcriber";
+import {translatorOnUpdate} from "./translator";
 
-export {transcribeOnCreate};
+export {transcribeOnCreate, translatorOnUpdate};
 
 admin.initializeApp();
